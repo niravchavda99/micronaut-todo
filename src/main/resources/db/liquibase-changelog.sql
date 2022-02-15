2b388124423ac9dc18967a6626c2bb10923770db
Nirav Chavda <nmchavda99@gmail.com>
2022-02-15 11:08:23 +0530

    Adds support for Test Containers with MariaDB

--- src/main/resources/db/liquibase-changelog.sql
+++ src/main/resources/db/liquibase-changelog.sql
@@ -1,3 +1,3 @@
 --liquibase formatted sql
 --changeset KartikNirav:1
-CREATE TABLE todo (id bigint(20) NOT NULL AUTO_INCREMENT, status int(11) DEFAULT NULL, title varchar(255) DEFAULT NULL, PRIMARY KEY (id));
+CREATE TABLE Todo (id bigint(20) NOT NULL AUTO_INCREMENT, status int(11) DEFAULT NULL, title varchar(255) DEFAULT NULL, PRIMARY KEY (id));
